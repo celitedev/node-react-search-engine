3708ec5660e9ef091aae6578492fa8043d0938d3
celite dev <iagilelite@gmail.com>
2016-04-23 19:21:50 +0200

    updates

--- src/components/NewCollection/CollectionCardSearch.js
+++ src/components/NewCollection/CollectionCardSearch.js
@@ -3,8 +3,8 @@ import PureComponent from '../../../node_modules/react-pure-render/component';
 import {connect} from 'redux-simple';
 import classnames from 'classnames';
 import _ from 'lodash';
-import {getCards, addCardToCollection, getCardsSuggestions} from '../../actions';
-import {Radio, RadioGroup, Textfield, Button} from 'react-mdl';
+import {getCards, addCardToCollection, getCardsSuggestions, deleteCardFromCollection} from '../../actions';
+import {Radio, RadioGroup, Button} from 'react-mdl';
 import 'material-design-icons';
 import Autosuggest from 'react-autosuggest';
 
@@ -22,18 +22,21 @@ function renderSuggestion(suggestion) {
 }
 
 
-@connect(searchedCards, {getCards, addCardToCollection, getCardsSuggestions})
+@connect(searchedCards, {getCards, addCardToCollection, deleteCardFromCollection, getCardsSuggestions})
 export default class CollectionCardSearch extends PureComponent {
   constructor(props, context) {
     super(props, context);
     this.state = {
       filter: 'all',
       cards: [],
-      cardTypes: ['place', 'event', 'person', 'all'],
+      cardTypes: ['place', 'event', 'person'],
       value: '',
       suggestions: [],
       noSuggestions: false
     };
+  }
+
+  componentDidMount() {
     this.loadCards();
   }
 
@@ -75,6 +78,7 @@ export default class CollectionCardSearch extends PureComponent {
     });
   }
 
+
   async loadCards() {
     const {getCards} = this.props;
     const cards = await getCards();
@@ -88,15 +92,31 @@ export default class CollectionCardSearch extends PureComponent {
     });
   }
 
-  filterResults(obj, type, search) {
+  filterResults(obj, type, search, filter) {
     const escapedValue = search.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
     const regex = new RegExp('^' + escapedValue, 'i');
     const filterArray = obj.filter(card => regex.test(card.title) && (card.type === type || type === 'all'));
+    if (filter && filter !== 'all') {
+      return _.filter(filterArray, {'type': filter});
+    }
     return filterArray;
   }
 
+  findCardById(id) {
+    return _.find(this.props.savedCollectionInfo.cards, {'id': id});
+  }
+
+  searchResults(e) {
+    e.preventDefault();
+    this.filterResults(
+      this.state.cards,
+      this.state.filter,
+      this.state.value
+    );
+  }
+
   render() {
-    const {className, addCardToCollection} = this.props;
+    const {className, addCardToCollection, savedCollectionInfo, deleteCardFromCollection} = this.props;
     const {cards, cardTypes, filter, value, suggestions, noSuggestions} = this.state;
     const searchFieldProps = {
       placeholder: 'Seach cards',
@@ -106,39 +126,42 @@ export default class CollectionCardSearch extends PureComponent {
     return (
       <div className={classnames(className, styles.root)}>
         <div id='cardsDialogStickyHeader'>
-          <RadioGroup value={filter} name='searchFilter'>
-            <Radio className={styles.radio} value='all' onClick={() => this.changeFilter('all')}>
-              All types
-            </Radio>
-            <Radio className={styles.radio} value='place' onClick={() => this.changeFilter('place')}>
-              Place
-            </Radio>
-            <Radio className={styles.radio} value='event' onClick={() => this.changeFilter('event')}>
-              Event
-            </Radio>
-            <Radio className={styles.radio} value='person' onClick={() => this.changeFilter('person')}>
-              Person
-            </Radio>
-          </RadioGroup>
-          <Autosuggest suggestions={suggestions}
-                       onSuggestionsUpdateRequested={::this.onSuggestionsUpdateRequested}
-                       getSuggestionValue={::this.getSuggestionValue}
-                       renderSuggestion={renderSuggestion}
-                       inputProps={searchFieldProps} />
-          {
-            noSuggestions &&
-            <div className='no-suggestions'>
-              No suggestions
-            </div>
-          }
-          <Button
-            className={classnames('mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect', styles.extraFiltersBtn)}>
-            Show Extra Filters
-          </Button>
+          <form onsubmit={::this.searchResults}>
+            <RadioGroup value={filter} name='searchFilter'>
+              <Radio className={styles.radio} value='all' onClick={() => this.changeFilter('all')}>
+                All types
+              </Radio>
+              <Radio className={styles.radio} value='place' onClick={() => this.changeFilter('place')}>
+                Place
+              </Radio>
+              <Radio className={styles.radio} value='event' onClick={() => this.changeFilter('event')}>
+                Event
+              </Radio>
+              <Radio className={styles.radio} value='person' onClick={() => this.changeFilter('person')}>
+                Person
+              </Radio>
+            </RadioGroup>
+            <Autosuggest suggestions={suggestions}
+                         onSuggestionsUpdateRequested={::this.onSuggestionsUpdateRequested}
+                         getSuggestionValue={::this.getSuggestionValue}
+                         renderSuggestion={renderSuggestion}
+                         inputProps={searchFieldProps} />
+            <Button
+              className={classnames('mdl-button mdl-js-button mdl-button--raised mdl-button--colored mdl-js-ripple-effect', styles.extraFiltersBtn)}
+              onClick={::this.searchResults}>
+              Show Cards
+            </Button>
+            {
+              noSuggestions &&
+              <div className='no-suggestions'>
+                No suggestions
+              </div>
+            }
+          </form>
         </div>
         { cardTypes.map((type, i) => (
           <div key={i}>
-            {(::this.filterResults(cards, type, value) && filter === type) ? (
+            {(_.find(cards, {'type': type}) && filter === 'all') || _.find(cards, {'type': type}) && filter === type ? (
               <li className={classnames('mdl-list__item', styles.collectionList)}>
                 <div className={classnames('mdl-card', styles.card)}>
                   <label htmlFor='' className={styles.collectionFilterName}>{type}</label>
@@ -151,20 +174,34 @@ export default class CollectionCardSearch extends PureComponent {
                         <img className={styles.cardImg} src='http://placehold.it/350x150'/>
                       </div>
                       <div className={classnames('mdl-card__supporting-text', styles.cardActions)}>
-                        <Button
-                          className={classnames(styles.extraFiltersBtn, styles.cardActionButton, 'mdl-js-ripple-effect')}
+                        {::this.findCardById(card.id) ?
+                          <Button
+                          accent
+                          className={classnames(styles.cardActionButton, 'mdl-js-ripple-effect button--colored')}
+                          onClick={() => deleteCardFromCollection(::this.findCardById(card.id).collectionId, card.id)}
+                          >
+                          Remove card
+                          </Button>
+                          :
+                          < Button
+                          colored
+                          className={classnames(styles.cardActionButton, 'mdl-js-ripple-effect')}
                           onClick={() => addCardToCollection(Math.random() * 10, card)}
-                        >
-                          Action
-                        </Button>
+                          >
+                          Add card
+                          </Button>
+                        }
                       </div>
                     </div>
                   )}
-                  <ul className={classnames('pagination', styles.pagination)}>
+                  {::this.filterResults(cards, type, value).length ?
+                    < ul className={classnames('pagination', styles.pagination)}>
                     <li className='mdl-button mdl-js-button mdl-button--icon'><a href='#!'>1</a></li>
-                    <li className='mdl-button mdl-js-button mdl-button--icon'><a href='#!'>2</a></li>
-                    <li className='mdl-button mdl-js-button mdl-button--icon'><a href='#!'>3</a></li>
-                  </ul>
+                    </ul>
+                  : <div className={styles.noResults}>
+                    No results
+                  </div>
+                  }
                 </div>
               </li>
             ) : null }
