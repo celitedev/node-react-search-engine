dfae61ece7427e1f6da2b22beb1018932e07d159
celite dev <iagilelite@gmail.com>
2016-05-16 16:05:00 +0200

    answer component fix

--- src/components/Answer/Answer.js
+++ src/components/Answer/Answer.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import PureComponent from 'react-pure-render/component';
-import ContentEditable from 'react-contenteditable';
 import {connect} from 'redux-simple';
 import { Link } from 'react-router';
 import _ from 'lodash';
 import classnames from 'classnames';
 
 import {redirect} from '../../actions';
+
+import CardsCarousel from '../Cards/CardsCarousel.js';
 
 @connect(null, {redirect})
 export default class Answer extends PureComponent {
@@ -15,13 +16,32 @@ export default class Answer extends PureComponent {
   }
 
   render() {
+    const {results} = this.props.answer;
+    const carouselSettings = {
+      slidesToShow: 3,
+      slidesToScroll: 3,
+    };
     return (
       <main className='mdl-layout__content'>
         <div className='page-content'>
           <div className='l-answerPage'>
-              <div id='js-carouselContainer'>
-                Answer here
+          {_.map(results, (answer, index) => {
+            return (
+              <div key={index}>
+                <div className='related-answer-text'>
+                  <ul>
+                    <li>
+                      {answer.answerNLP}
+                    </li>
+                    <li>
+                      <a href=''>View</a>
+                    </li>
+                  </ul>
+                </div>
+                <CardsCarousel settings={carouselSettings} results={answer.results} cardsStyle={styles.sliderCard}/>
               </div>
+              );
+          })}
           </div>
         </div>
       </main>
