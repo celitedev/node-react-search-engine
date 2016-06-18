019c29f649b68bafbb13342b53f79c9b71330c8e
celite dev <iagilelite@gmail.com>
2016-06-18 18:55:55 +0200

    close #90

--- src/components/Html.js
+++ src/components/Html.js
@@ -45,15 +45,21 @@ export default class Html extends React.Component {
         <meta name='viewport' content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no'/>
         <meta name='msapplication-TileColor' content='#ffffff'/>
         <meta name='theme-color' content='#ffffff'/>
-        <script dangerouslySetInnerHTML={{__html: hydrate}} defer/>
-        <script crossOrigin='anonymous' src={commonBundle.publicPath} defer/>
-        <script crossOrigin='anonymous' src={clientBundle.publicPath} defer/>
+        <script crossOrigin='anonymous' dangerouslySetInnerHTML={{__html: `
+          !function(){var analytics=window.analytics=window.analytics||[];if(!analytics.initialize)if(analytics.invoked)window.console&&console.error&&console.error("Segment snippet included twice.");else{analytics.invoked=!0;analytics.methods=["trackSubmit","trackClick","trackLink","trackForm","pageview","identify","group","track","ready","alias","page","once","off","on"];analytics.factory=function(t){return function(){var e=Array.prototype.slice.call(arguments);e.unshift(t);analytics.push(e);return analytics}};for(var t=0;t<analytics.methods.length;t++){var e=analytics.methods[t];analytics[e]=analytics.factory(e)}analytics.load=function(t){var e=document.createElement("script");e.type="text/javascript";e.async=!0;e.src=("https:"===document.location.protocol?"https://":"http://")+"cdn.segment.com/analytics.js/v1/"+t+"/analytics.min.js";var n=document.getElementsByTagName("script")[0];n.parentNode.insertBefore(e,n)};analytics.SNIPPET_VERSION="3.0.1"; //eslint-disable-line no-use-before-define
+          analytics.load("sQZAVxCPmZBY6wTWFqM6vlArgT6O5Drm");
+          analytics.page();
+          }}();
+        `}} />
       </head>
       <body>
-        {commonCssBundle && <link rel='stylesheet' href={commonCssBundle.publicPath}/>}
-        {clientCssBundle && <link rel='stylesheet' href={clientCssBundle.publicPath}/>}
+      {commonCssBundle && <link rel='stylesheet' href={commonCssBundle.publicPath}/>}
+      {clientCssBundle && <link rel='stylesheet' href={clientCssBundle.publicPath}/>}
 
-        <div id='react-app' dangerouslySetInnerHTML={{__html: markup}}/>
+      <div id='react-app' dangerouslySetInnerHTML={{__html: markup}}/>
+      <script dangerouslySetInnerHTML={{__html: hydrate}}/>
+      <script crossOrigin='anonymous' src={commonBundle.publicPath}/>
+      <script crossOrigin='anonymous' src={clientBundle.publicPath}/>
       </body>
       </html>
     );
