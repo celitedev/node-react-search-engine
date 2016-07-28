634f86b150434456b2235fb6192a69c89b26c959
celite dev <iagilelite@gmail.com>
2016-07-27 21:31:44 -0400

    Issue133: add favicon (#143)
    This commit closes issue.

--- src/components/Html.js
+++ src/components/Html.js
@@ -45,6 +45,8 @@ export default class Html extends React.Component {
         <meta name='viewport' content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no'/>
         <meta name='msapplication-TileColor' content='#ffffff'/>
         <meta name='theme-color' content='#ffffff'/>
+        <link rel='shortcut icon' type='image/x-icon' href={require('../images/favicon.png')} />
+
         <script crossOrigin='anonymous' dangerouslySetInnerHTML={{__html: `
           !function(){var analytics=window.analytics=window.analytics||[];if(!analytics.initialize)if(analytics.invoked)window.console&&console.error&&console.error("Segment snippet included twice.");else{analytics.invoked=!0;analytics.methods=["trackSubmit","trackClick","trackLink","trackForm","pageview","identify","group","track","ready","alias","page","once","off","on"];analytics.factory=function(t){return function(){var e=Array.prototype.slice.call(arguments);e.unshift(t);analytics.push(e);return analytics}};for(var t=0;t<analytics.methods.length;t++){var e=analytics.methods[t];analytics[e]=analytics.factory(e)}analytics.load=function(t){var e=document.createElement("script");e.type="text/javascript";e.async=!0;e.src=("https:"===document.location.protocol?"https://":"http://")+"cdn.segment.com/analytics.js/v1/"+t+"/analytics.min.js";var n=document.getElementsByTagName("script")[0];n.parentNode.insertBefore(e,n)};analytics.SNIPPET_VERSION="3.0.1"; //eslint-disable-line no-use-before-define
           analytics.load("sQZAVxCPmZBY6wTWFqM6vlArgT6O5Drm");
