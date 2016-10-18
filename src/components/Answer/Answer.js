3a7001118579f3f156f818edb6ee4b5ff9caa4d6
celite dev <iagilelite@gmail.com>
2016-10-18 14:17:59 +0200

    update answer page step 1

--- src/components/Answer/Answer.js
+++ src/components/Answer/Answer.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import {map} from 'lodash';
 import CardsCarousel from '../Cards/CardsCarousel.js';
+import CardsPagination from '../Cards/CardPagination.js';
+import Card from '../Cards/Card.js';
 import classnames from 'classnames';
 import {pure, compose, mapProps} from 'recompose';
 import {answerCarousel} from '../componentsConfig/cardsCarousel';
@@ -11,29 +13,31 @@ import {Link} from 'react-router';
 const enhance = compose(
   mapProps(
     props => {
-      const {answer, params} = props;
-      return {answer, params};
+      const {answer, params, mainTab, subTab} = props;
+      return {answer, params, mainTab, subTab};
     }
   ),
   pure
 );
 
-const Answer = enhance(({answer, params}) => (
+const Answer = enhance(({answer, params, mainTab, subTab}) => (
 
   <main className='mdl-layout__content'>
     <div className='page-content'>
       <div className='l-answerPage'>
         {map(answer.results, (answer, index) => {
-          return answer.results.length > 0 && (
-              <div key={index}>
+          return answer.results.length > 0 && index === mainTab && (
+              <div key={index} className={styles.paginationSection}>
                 <div className={classnames('related-answer-text', styles.topicHeader)}>
                   <Link dangerouslySetInnerHTML={{__html: answer.answerNLP}} to={`/search/${params.question}`} query={{ filter: JSON.stringify(answer.filterContext) }}/>
                 </div>
-                <CardsCarousel settings={answerCarousel} showAll={true} question={params.question} filterContext={answer.filterContext} results={answer.results}
-                               cardsStyle={styles.sliderCard} answer={answer}/>
+                {/* <CardsCarousel settings={answerCarousel} showAll={true} question={params.question} filterContext={answer.filterContext} results={answer.results}
+                               cardsStyle={styles.sliderCard} answer={answer}/> */}
+               <CardsPagination settings={answerCarousel} showAll={true} question={params.question} filterContext={answer.filterContext} results={answer.results}
+                              cardsStyle={styles.paginationCard} answer={answer}/>
               </div>
             );
-        }) }
+        })}
       </div>
     </div>
     <Footer />
